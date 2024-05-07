8c511f37afd48e8457b1a25f441eef54bdd0d346
Daniel <danielimbert90@gmail.com>
2024-05-07 09:47:53 +0300

    Setting up ColorChange functionality

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -13,11 +13,15 @@ public class MenuUIHandler : MonoBehaviour
     public ColorPicker ColorPicker;
     public Button StartButton;
 	public Button ExitButton;
+	public Button SaveColorButton;
+	public Button LoadColorButton;
+
+	private const string ColorKey = "SelectedColor";
 
 	public void NewColorSelected(Color color)
-    {
-        // add code here to handle when a color is selected
-    }
+	{
+		// No need to save the color immediately, as the ColorPicker component should handle it internally
+	}
 
 	public void StartButtonClicked()
 	{
@@ -26,15 +30,54 @@ public class MenuUIHandler : MonoBehaviour
 
 	public void ExitButtonClicked()
 	{
-		// Close the application
+		
 		Application.Quit();
 	}
 
 
 	private void Start()
     {
-        ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
-        ColorPicker.onColorChanged += NewColorSelected;
-    }
+        
+		ColorPicker.Init();
+		ColorPicker.onColorChanged += NewColorSelected;
+		LoadSelectedColor();
+
+	}
+
+	public void SaveColorButtonClicked()
+	{
+		// Save the currently selected color from the ColorPicker
+		SaveSelectedColor(ColorPicker.CurrentColor);
+	}
+
+	public void LoadColorButtonClicked()
+	{
+		// Load the saved color and apply it to the ColorPicker
+		LoadSelectedColor();
+	}
+
+	void SaveSelectedColor(Color color)
+	{
+		// Save the selected color to PlayerPrefs
+		PlayerPrefs.SetFloat(ColorKey + "_R", color.r);
+		PlayerPrefs.SetFloat(ColorKey + "_G", color.g);
+		PlayerPrefs.SetFloat(ColorKey + "_B", color.b);
+		PlayerPrefs.SetFloat(ColorKey + "_A", color.a);
+		PlayerPrefs.Save();
+	}
+
+	 void LoadSelectedColor()
+	{
+		// Load the selected color from PlayerPrefs
+		if (PlayerPrefs.HasKey(ColorKey + "_R"))
+		{
+			float r = PlayerPrefs.GetFloat(ColorKey + "_R");
+			float g = PlayerPrefs.GetFloat(ColorKey + "_G");
+			float b = PlayerPrefs.GetFloat(ColorKey + "_B");
+			float a = PlayerPrefs.GetFloat(ColorKey + "_A");
+			Color selectedColor = new Color(r, g, b, a);
+
+			
+		}
+	}
 }
