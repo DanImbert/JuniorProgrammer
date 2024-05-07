a968b2c23e31f34915fe8dbc46657240c58aa49f
Daniel <danielimbert90@gmail.com>
2024-05-07 13:00:34 +0300

    Add Data persistence between sessions, add save data class to json file, add save color method, add load color method, implement test buttons.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -12,12 +12,12 @@ using UnityEditor;
 public class MenuUIHandler : MonoBehaviour
 {
 	public ColorPicker ColorPicker;
-	public Button StartButton;
-	public Button ExitButton;
-	public Button SaveColorButton;
-	public Button LoadColorButton;
+	//public Button StartButton;
+	//public Button ExitButton;
+	//public Button SaveColorButton;
+	//public Button LoadColorButton;
 
-	private const string ColorKey = "SelectedColor";
+	//private const string ColorKey = "SelectedColor";
 
 	public void NewColorSelected(Color color)
 	{
@@ -28,21 +28,23 @@ public class MenuUIHandler : MonoBehaviour
 
 	public void SaveColorButtonClicked()
 	{
-		// Save the currently selected color from the ColorPicker
-		SaveSelectedColor(ColorPicker.SelectedColor);
+		MainManager.Instance.SaveColor();
+
 	}
 
 	public void LoadColorButtonClicked()
 	{
-		// Load the saved color and apply it to the ColorPicker
-		LoadSelectedColor();
+		MainManager.Instance.LoadColor();
+		ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+
 	}
 
 	private void Start()
 	{
 		ColorPicker.Init();
 		ColorPicker.onColorChanged += NewColorSelected;
-		
+		ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+
 	}
 
 	public void StartNew()
@@ -52,37 +54,17 @@ public class MenuUIHandler : MonoBehaviour
 
 	public void ExitButtonClicked()
 	{
+		
+		MainManager.Instance.SaveColor();
+
      #if UNITY_EDITOR
 		EditorApplication.ExitPlaymode();
      #else
         Application.Quit(); // original code to quit Unity player
      #endif
+
+      
 	}
 
-	private void SaveSelectedColor(Color color)
-	{
-		// Save the selected color to PlayerPrefs
-		PlayerPrefs.SetFloat(ColorKey + "_R", color.r);
-		PlayerPrefs.SetFloat(ColorKey + "_G", color.g);
-		PlayerPrefs.SetFloat(ColorKey + "_B", color.b);
-		PlayerPrefs.SetFloat(ColorKey + "_A", color.a);
-		PlayerPrefs.Save();
-	}
-
-	private void LoadSelectedColor()
-	{
-		// Load the selected color from PlayerPrefs
-		if (PlayerPrefs.HasKey(ColorKey + "_R"))
-		{
-			float r = PlayerPrefs.GetFloat(ColorKey + "_R");
-			float g = PlayerPrefs.GetFloat(ColorKey + "_G");
-			float b = PlayerPrefs.GetFloat(ColorKey + "_B");
-			float a = PlayerPrefs.GetFloat(ColorKey + "_A");
-			Color selectedColor = new Color(r, g, b, a);
-
-			// Apply the loaded color to the ColorPicker
-			ColorPicker.SelectColor(selectedColor);
-			
-		}
-	}
+	
 }
